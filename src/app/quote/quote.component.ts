b988ac6a1899638a1401c896922b5fdd61fa764d
PerisOduol618 <oduolpepe618@gmail.com>
2020-08-23 11:55:34 +0300

    Pass an event emmiter as an argument

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,20 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote('Walking on water and developing software from a specification are easy if both are frozen.', 'Marvin Minsky'),
-    new Quote('You can only become truly accomplished at something you love.', 'Maya Angelou'),
-    new Quote('A conscious human is driven by their conscience, not popular opinion.', 'Paul Graham'),
-
+    new Quote('Walking on water and developing software from a specification are easy if both are frozen.', 'Marvin Minsky',new Date(2020,3,14)),
+    new Quote('You can only become truly accomplished at something you love.', 'Maya Angelou',new Date(2020,6,9)),
+    new Quote('A conscious human is driven by their conscience, not popular opinion.', 'Paul Graham',new Date(2022,1,12)),
 
   ];
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].text}?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
 
   constructor() { }
 
